17cf75c1eb22008205b06c4f94bc7363e7f4e23d
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-11-26 18:17:51 -0300

    feat: create new deposit functionality

--- src/index.js
+++ src/index.js
@@ -56,4 +56,20 @@ app.get('/statements', verifyIfExistsAccountCPF, (request, response) => {
   return response.json(customer.statement);
 });
 
+app.post('/deposits', verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit',
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.sendStatus(201);
+});
+
 app.listen(port, () => console.log(`Listening on port: ${port}`));
