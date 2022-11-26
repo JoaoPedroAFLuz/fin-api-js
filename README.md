65b13547884d9bce34bdc11d81783c028c4e9885
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-11-26 16:56:20 -0300

    feat: create new account functionality

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ###  ✅ Requisitos
 
-- [ ] Deve ser possível criar uma conta;
+- [x] Deve ser possível criar uma conta;
 - [ ] Deve ser possível buscar o extrato bancário do cliente;
 - [ ] Deve ser possível realizar um depósito;
 - [ ] Deve ser possível realizar um saque;
@@ -13,7 +13,7 @@
 
 ### 👩🏽‍💼 Regras de negócio
 
-- [ ] Não deve ser possível cadastrar uma conta com CPF já existente;
+- [x] Não deve ser possível cadastrar uma conta com CPF já existente;
 - [ ] Não deve ser possível fazer depósito em uma conta não existente;
 - [ ] Não deve ser possível buscar extrato em uma conta não existente;
 - [ ] Não deve ser possível fazer saque em uma conta não existente;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const port = 3333;
 const app = express();
+app.use(express.json());
 
-app.get('/', (request, response) => {
-  response.send('Hello World!');
+const customers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - []
+ */
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    response.status(400).json({ error: 'Customer already exists' });
+  }
+
+  const id = uuidv4();
+
+  customers.push({
+    id,
+    cpf,
+    name,
+    statement: [],
+  });
+
+  response.sendStatus(201);
 });
 
 app.listen(port, () => console.log(`Listening on ${port}`));
